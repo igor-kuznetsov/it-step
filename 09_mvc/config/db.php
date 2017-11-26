eb3cf304f1d28dc1c96d548b642b83ca08a18cd2
igor-kuznetsov <webdevastator@gmail.com>
2017-11-26 13:27:09 +0200

    mvc

--- 09_mvc/config/db.php
+++ 09_mvc/config/db.php
@@ -5,4 +5,4 @@ use core\Config;
 Config::set('db_host', 'localhost');
 Config::set('db_name', 'my_db2');
 Config::set('db_user', 'root');
-Config::set('db_pass', 'MD56kq');
+Config::set('db_pass', '');
