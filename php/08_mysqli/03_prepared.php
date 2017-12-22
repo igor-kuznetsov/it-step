75dc8d4525d3bbd961fa7eed16cecb8275ab1405
igor-kuznetsov <webdevastator@gmail.com>
2017-12-22 19:12:05 +0200

    web - mysqli

--- php/08_mysqli/03_prepared.php
+++ php/08_mysqli/03_prepared.php
@@ -8,33 +8,43 @@ if ($mysqli->connect_errno) {
     die("Failed to connect: " . $mysqli->connect_error);
 }
 
-$sql = "INSERT INTO `products` VALUES (?, ?, ?);";
+$sql = "INSERT INTO `products` (`name`,`price`,`category_id`) VALUES (?, ?, ?);";
 $statement = $mysqli->prepare($sql);
 
 if (!$statement) {
     die("Preparation failed: " . $mysqli->error);
 }
 
-$statement->bind_param("s", $product_name); // string
-$statement->bind_param("d", $product_price); // double
-$statement->bind_param("i", $product_category); // integer
+if (!$statement->bind_param("sdi", $product_name, $product_price, $product_category)) {
+    die("Binding parameters failed: " . $statement->error);
+}
 
 $product_name = 'Product 1';
 $product_price = 128.56;
 $product_category = 1;
 
-$statement->execute();
+if (!$statement->execute()) {
+    die("Execute failed: " . $statement->error);
+}
 
 
 $product_name = 'Product 2';
 $product_price = 145.78;
 $product_category = 2;
-$statement->execute();
+
+if (!$statement->execute()) {
+    die("Execute failed: " . $statement->error);
+}
+
 
 $product_name = 'Product 3';
 $product_price = 98.12;
 $product_category = 3;
-$statement->execute();
 
+if (!$statement->execute()) {
+    die("Execute failed: " . $statement->error);
+}
 
 $statement->close();
+
+echo "OK";
